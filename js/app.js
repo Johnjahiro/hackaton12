b23978d98db4008eb6db0a4555b976cbdd3d2204
jairo <jairo_cz01@hotmail.com>
2020-06-20 11:20:33 -0500

    menu acordion

--- js/app.js
+++ js/app.js
@@ -2,26 +2,46 @@ class Dropdown {
   constructor() {
     const dropBtn = document.getElementById("dropBtn");
     const dropdownContent = document.getElementById("dropdownContent");
+    // const btnAcordion = document.getElementById("btnAcordion")
+    // const acordionContent = document.getElementById("acordionContent")
 
     dropdownContent.style.display = "none";
 
-    this.show();
-    this.hide();
+    this.showMenu();
+    this.hideMenu();
+    this.showAcordion();
   }
 
-  show() {
+  showMenu() {
     dropBtn.addEventListener("mouseover", () => {
       if (dropdownContent.style.display === "none") {
         dropdownContent.style.display = "block";
-        dropdownContent.style.transition = "display .5s easy"
+        dropdownContent.style.transition = "display .5s easy";
         return;
       }
     });
   }
 
-  hide() {
+  hideMenu() {
     dropBtn.addEventListener("mouseout", () => {
       dropdownContent.style.display = "none";
+    });
+  }
+
+  showAcordion() {
+    const acordionContent = document.getElementById("acordionContent");
+    const menuAcordion = document.getElementById("menuAcordion");
+
+    menuAcordion.addEventListener("click", (e) => {
+      // console.log(e)
+
+      let sibling = e.target.nextElementSibling;
+
+      if (sibling.style.display === "block") {
+        sibling.style.display = "none";
+      } else {
+        sibling.style.display = "block";
+      }
     });
   }
 }
